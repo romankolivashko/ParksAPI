9ad5310bfa80d1f57f53fb4ce11d0b889c2cd190
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-29 14:19:01 -0700

    WIP: readme

--- ParksApi/Migrations/20211029205558_UpdateFloridaState.Designer.cs
+++ ParksApi/Migrations/20211029205558_UpdateFloridaState.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ParksApi.Models;
 
 namespace AirportCodes.Migrations
 {
     [DbContext(typeof(ParksApiContext))]
-    partial class ParksApiContextModelSnapshot : ModelSnapshot
+    [Migration("20211029205558_UpdateFloridaState")]
+    partial class UpdateFloridaState
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ParksApi/Migrations/20211029205558_UpdateFloridaState.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AirportCodes.Migrations
+{
+    public partial class UpdateFloridaState : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "State",
+                value: "Florida");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 6,
+                column: "State",
+                value: "California");
+        }
+    }
+}
