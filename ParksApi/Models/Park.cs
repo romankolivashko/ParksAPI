666b3c3815f0d7885c82980e3b5b5b74d0452f80
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-29 09:02:57 -0700

    Added seeding data

--- ParksApi/Models/Park.cs
+++ ParksApi/Models/Park.cs
@@ -6,8 +6,9 @@ namespace ParkApi.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string State { get; set; }
+        public string StateId { get; set; }
         public string Area { get; set; }
         public string Visitors { get; set; }
-        public string DateEstablished { get; set; }
+        public string YearEstablished { get; set; }
     }
 }
