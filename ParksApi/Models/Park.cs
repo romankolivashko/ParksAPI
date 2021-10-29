d19fd49356fc5cf4db6e7f17e123e4406ef3241a
Roman Kolivashko <rkolivashko@gmail.com>
2021-10-29 08:33:27 -0700

    Added Park model

---
+++ ParksApi/Models/Park.cs
@@ -0,0 +1,13 @@
+namespace ParkApi.Models
+{
+    public class Park
+    {
+        public int ParkId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public string Area { get; set; }
+        public string Visitors { get; set; }
+        public string DateEstablished { get; set; }
+    }
+}
